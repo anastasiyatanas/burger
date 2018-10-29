717a41cd95a89b244eb9738e2edafa548f1772b7
Anastasiya Tanas <Anastasiya@MacBook.localdomain>
2018-10-29 14:44:45 -0600

    unfinished

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Grilled chicken burger');
+INSERT INTO burgers (name) VALUES ('beef burger');
+INSERT INTO burgers (name, devour) VALUES ('Royal Burger', true);
+INSERT INTO burgers (name, devour) VALUES ('Bacon  Burger', true);
+INSERT INTO burgers (name, devour) VALUES ('Bison Burger', true);
+INSERT INTO burgers (name) VALUES ('Avocado Burger');
